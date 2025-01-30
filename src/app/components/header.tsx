b545f9fcefcdd99bf8737908022cb7c4a4069417
joggest95 <jilienthal@gmail.com>
2025-01-30 12:07:15 -0500

    swapped <a> with <Link>

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link';
+
 export default function Header() {
     return (
         <div id="header"
             className="bg-white w-full h-1/8 flex items-center justify-around fixed top-0 z-10 opacity-65">
-            <a href="/">
+            <Link href="/">
                 <img src="/sitelogo.png"
                     alt="J. Brescia Travel Co."
                     className="h-20 w-auto md:h-32 md:w-auto">
                 </img>
-            </a>
+            </Link>
             <div className="text-black">
                 <div id="phone">
                     (727) 266-JBTC
