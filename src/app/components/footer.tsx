b13a8bf32e98e3027af65bc2a228dffe43c661c6
joggest95 <jilienthal@gmail.com>
2025-02-06 14:44:13 -0500

    capitalization issue

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -5,7 +5,7 @@ export default function Footer() {
         <div id="footer" className="flex flex-row w-full h-1/3 snap-start snap-always bg-white">
             <div id="presenting" className="flex flex-row h-full w-2/3 gap-10 items-end bg-red-400">
                 <img alt="Disney Logo" src="/presenting/disneylogo.gif" className={logoClass}></img>
-                <img alt="Disney Travel Logo"  src="/presenting/walt_disney_travel.png" className={logoClass}></img>
+                <img alt="Disney Travel Logo"  src="/presenting/Walt_Disney_travel.png" className={logoClass}></img>
                 <img alt="Universal Logo" src="/presenting/UPRV-Logo-2018.jpg" className={logoClass}></img>
                 <img alt="Apple Vacations Logo" src="/presenting/apple-vacations.webp" className={logoClass}></img>
                 <img alt="Seabourn Logo" src="/presenting/seabournlogo.gif" className={logoClass}></img>
