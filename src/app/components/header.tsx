973c8fcac5bc8cacecad063b60aaf0b21a8a464d
joggest95 <jilienthal@gmail.com>
2025-02-22 17:15:04 -0500

    adjusted header contact icon sizing and styling. adjusted header height

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -5,19 +5,19 @@ import { IoMdMail } from "react-icons/io";
 export default function Header() {
     return (
         <div id="header"
-            className="w-full flex items-center justify-around fixed top-0 z-10 md:py-2 py-4 px-2 max-h-30 overflow-x-hidden">
+            className="w-full flex items-center justify-around fixed top-0 z-10  px-2 max-h-30 overflow-x-hidden">
             <div className='absolute inset-0 bg-white opacity-65 z-0'></div>
             <Link href="#video+tag">
                 <img src="/JBTC-Logo-v2Noborder.png"
                     alt="J. Brescia Travel Co."
-                    className="max-h-20 md:max-h-28 w-auto relative z-10 hover:opacity-60">
+                    className="max-h-20 md:max-h-24 w-auto relative z-10 hover:opacity-55">
                 </img>
             </Link>
-            <div id="contactAndButtons" className="text-black flex flex-row md:gap-8 gap-4 items-center justify-between relative z-10">
-                <div id="contactInfoCont">
+            <div id="contactAndButtons" className="text-black flex flex-row md:gap-8 gap-3 items-center justify-between relative z-10">
+                <div id="contactInfoCont" className='flex flex-row md:flex-col'>
                     <div id="phone" className='flex flex-row items-center gap-2 hover:text-gray-600'>
                         <Link href="tel:7272665282">
-                            <FaSquarePhone className="text-3xl md:text-xl"/>
+                            <FaSquarePhone className="text-[38px] md:text-2xl"/>
                         </Link>
                         <Link className='hidden md:block' href="tel:7272665282">
                             (727) 266-JBTC
@@ -25,7 +25,7 @@ export default function Header() {
                     </div>
                     <div id="email" className='flex flex-row items-center gap-2 hover:text-gray-600'>
                         <Link href="mailto:JBTravelCo@icloud.com">
-                            <IoMdMail className="text-3xl md:text-xl" />
+                            <IoMdMail className="text-5xl md:text-2xl" />
                         </Link>
                         <Link className='hidden md:block' href="mailto:JBTravelCo@icloud.com">
                             JBTravelCo@icloud.com
@@ -34,11 +34,11 @@ export default function Header() {
                 </div>
                 <div id="buttonContainer" className='flex flew-row md:gap-5 gap-3'>
                     <Link href="#planTrip">
-                        <button className='text-sm md:text-md rounded-xl border-2 border-black border-opacity-30 md:p-4 p-2 hover:border-opacity-100 hover:bg-white hover:bg-opacity-40 text-nowrap'>
+                        <button className='text-sm md:text-md rounded-2xl border-2 border-black border-opacity-30 md:p-4 p-2 hover:border-opacity-100 hover:bg-white hover:bg-opacity-40 text-nowrap'>
                             Plan My Trip
                         </button>
                     </Link>
-                    <button className='text-sm md:text-md rounded-xl border-2 border-black border-opacity-30 md:p-4 p-2 hover:border-opacity-100 hover:bg-white hover:bg-opacity-40 text-nowrap'>
+                    <button className='text-sm md:text-md rounded-2xl border-2 border-black border-opacity-30 md:p-4 p-2 hover:border-opacity-100 hover:bg-white hover:bg-opacity-40 text-nowrap'>
                         <Link href="https://crm.myagentgenie.com/portal/login" target="_blank" rel="noopener noreferrer">
                             Login
                         </Link>
